00353feb2bf78a1eca5247b5dac411ebbfe5d849
Joshion <joshion@outlook.com>
2017-03-01 09:17:34 +0800

    change project's libs setting

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -5,7 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    StreamPlayProto::pbCliReqChangeOrientation orient;
+    orient.set_orientation(10);
+    orient.set_screenheight(640);
+    orient.set_screenwidth(480);
     MainFrame mainframe;
-
     return a.exec();
 }
