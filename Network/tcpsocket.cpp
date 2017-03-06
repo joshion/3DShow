7e85ef69b5cc7e9838cbe1ff911c65f4d25de7c4
Joshion <joshion@outlook.com>
2017-03-06 15:00:33 +0800

    add require and respone in tcpsocket

--- Network/tcpsocket.cpp
+++ Network/tcpsocket.cpp
@@ -1,48 +1,153 @@
 #include "tcpsocket.h"
 #include "framebuffer.h"
 
-#include <ConnectProto.pb.h>
-#include <KinectDataProto.pb.h>
+#include "ConnectProto.pb.h"
+#include "KinectDataProto.pb.h"
 
 #include <QtNetwork/QTcpSocket>
 #include <QString>
 
-#include <string>
 
 TcpSocket::TcpSocket(QString adress, int port, QObject *parent)
     : QObject(parent), m_strIPAdress(adress), m_uPort(port)
 {
+    m_pFrameBuffer = new FrameBuffer();
     m_pTcpSocket = new QTcpSocket(this);
-
     m_pTcpSocket->connectToHost(m_strIPAdress, m_uPort);
-
     connect(m_pTcpSocket, &QTcpSocket::readyRead, this, &TcpSocket::readDataFromServer);
 }
 
 TcpSocket::~TcpSocket()
 {
-    m_pTcpSocket->close();
-    delete m_pTcpSocket;
+    if (m_pTcpSocket)
+    {
+        m_pTcpSocket->close();
+        delete m_pTcpSocket;
+    }
+    if (m_pFrameBuffer)
+    {
+        delete m_pFrameBuffer;
+    }
 }
 
 bool TcpSocket::writeDataToServer()
 {
-    ConnectProto::pbRespConnect resp;
-    resp.set_failreason("failure");
-    resp.set_resulttype(1);
+    //requireConnect();
+    //_sleep(2000);
+    ////requireDevices();
+    //startConnect("hello", 1);
+    //_sleep(2000);
+    //endConnect();
+    //_sleep(2000);
+    exitConnect();
+    _sleep(2000);
+    return true;
+}
 
-    FrameBuffer buffer(1, 1, 0, 0, 0, nullptr);
-    buffer.setData(resp);
+bool TcpSocket::writeBufferToServer() const
+{
+    QByteArray bytes = FrameBuffer::toByte(*m_pFrameBuffer);
+    int writeLength = m_pTcpSocket->write(bytes);
+    return writeLength == bytes.length();
+}
+
+bool TcpSocket::writeBufferToServer(const FrameBuffer & buffer)
+{
     QByteArray bytes = FrameBuffer::toByte(buffer);
     int writeLength = m_pTcpSocket->write(bytes);
     return writeLength == bytes.length();
+}
+
+bool TcpSocket::requireConnect()
+{
+    m_pFrameBuffer->setCmdType(1);
+    m_pFrameBuffer->setCmdNum(1);
+    m_pFrameBuffer->setData(nullptr, 0);
+    return this->writeBufferToServer();
+}
+
+bool TcpSocket::exitConnect()
+{
+    m_pFrameBuffer->setCmdType(1);
+    m_pFrameBuffer->setCmdNum(2);
+    m_pFrameBuffer->setData(nullptr, 0);
+    return this->writeBufferToServer();
+}
+
+bool TcpSocket::requireDevices()
+{
+    m_pFrameBuffer->setCmdType(1);
+    m_pFrameBuffer->setCmdNum(3);
+    m_pFrameBuffer->setData(nullptr, 0);
+    return this->writeBufferToServer();
+}
+
+bool TcpSocket::startConnect(std::string deviceName, unsigned int dataType)
+{
+    KinectDataProto::pbReqStart req;
+    req.set_devicename(deviceName);
+    req.set_datatype(dataType);
+    m_pFrameBuffer->setData(req);
+    return this->writeBufferToServer();
+}
+
+bool TcpSocket::endConnect()
+{
+    m_pFrameBuffer->setCmdType(2);
+    m_pFrameBuffer->setCmdNum(2);
+    m_pFrameBuffer->setData(nullptr, 0);
+    return this->writeBufferToServer();
 }
 
 void TcpSocket::readDataFromServer()
 {
     QByteArray bytes = m_pTcpSocket->readAll();
     FrameBuffer buffer = FrameBuffer::fromByte(bytes);
-    ConnectProto::pbRespConnect resp;
-    resp.ParseFromArray(buffer.data(), buffer.length());
-    resp.PrintDebugString();
+    switch (buffer.cmdType())
+    {
+    case 1:
+        switch (buffer.cmdNum())
+        {
+        case 100:
+            {
+                ConnectProto::pbRespConnect resp;
+                resp.ParseFromArray(buffer.data(), buffer.length());
+                resp.PrintDebugString();
+            }
+            break;
+        case 101:
+            {
+                ConnectProto::pbRespDevices resp;
+                resp.ParseFromArray(buffer.data(), buffer.length());
+                resp.PrintDebugString();
+            }
+            break;
+        default:
+            break;
+        }
+        break;
+    case 2:
+        switch (buffer.cmdNum())
+        {
+        case 100:
+            {
+                KinectDataProto::pbReqStart req;
+                req.ParseFromArray(buffer.data(), buffer.length());
+                req.PrintDebugString();
+            }
+            break;
+        case 101:
+            {
+                KinectDataProto::pbRespStart resp;
+                resp.ParseFromArray(buffer.data(), buffer.length());
+                resp.PrintDebugString();
+            }
+            break;
+        default:
+            break;
+        }
+        break;
+    default:
+        break;
+    }
 }
--- Network/tcpsocket.h
+++ Network/tcpsocket.h
@@ -2,15 +2,16 @@
 #define TCPSOCKET_H
 
 #include <QObject>
+#include <string>
 
+class FrameBuffer;
 class QString;
 class QTcpSocket;
-class QTimer;
 
-using namespace std;
-
-const static QString SERVER_IPADRESS = "127.0.0.1";
-const static int SERVER_PORT = 7892;
+//using namespace std;
+//
+//const static QString SERVER_IPADRESS = "127.0.0.1";
+//const static int SERVER_PORT = 7892;
 
 class TcpSocket : public QObject
 {
@@ -23,9 +24,17 @@ private:
     QString m_strIPAdress;
     int m_uPort;
     QTcpSocket *m_pTcpSocket;
+    FrameBuffer *m_pFrameBuffer;
 
 public:
     bool writeDataToServer();
+    bool writeBufferToServer() const;
+    bool writeBufferToServer(const FrameBuffer & buffer);
+    bool requireConnect();
+    bool exitConnect();
+    bool requireDevices();
+    bool startConnect(std::string deviceName, unsigned int dataType);
+    bool endConnect();
 
 public slots:
     void readDataFromServer();
