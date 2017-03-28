9102dcbf7b43fe2a4986cc45a0ececff2a082e4d
Joshion <joshion@outlook.com>
2017-03-28 16:38:35 +0800

    improve to show the skeleton frame

--- GUI/framepainter.cpp
+++ GUI/framepainter.cpp
@@ -14,6 +14,7 @@ FramePainter::FramePainter()
 {
     m_pTriangleVertices = new QVector4D[6];
     m_pColor = new QVector4D[6];
+    m_pElement = new GLushort[8];
 }
 
 FramePainter::~FramePainter()
@@ -42,13 +43,12 @@ bool FramePainter::buildShaderProgram(const QString & strVertFile, const QString
         m_Program.setUniformValue(PROJECTION_LOCATION, projection_Matrix);
 
         glGenVertexArrays(1, m_VertexArraysObject);
-        glBindVertexArray(m_VertexArraysObject[0]);
         glGenBuffers(1, m_PointsBuffer);
-        glBindBuffer(GL_ARRAY_BUFFER, m_PointsBuffer[0]);
+        glGenBuffers(1, m_ElementBuffer);
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
-
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
     }
     return flag;
 }
@@ -58,25 +58,25 @@ void FramePainter::paint()
     m_Program.bind();
     loadFrame();
 
+    glBindVertexArray(m_VertexArraysObject[0]);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ElementBuffer[0]);
     glEnableVertexAttribArray(VERTEX_LOCATION);
     glEnableVertexAttribArray(COLOR_LOCATION);
 
-    glLineWidth(6.0);
-    glDrawArrays(GL_LINE_LOOP, 0, 3);
+
+    glDrawArrays(GL_POINTS, 0, 6);
+    glLineWidth(3.0);
+    glDrawElements(GL_LINE_STRIP, 8, GL_UNSIGNED_SHORT, nullptr);
     glFlush();
 
     glDisableVertexAttribArray(VERTEX_LOCATION);
     glDisableVertexAttribArray(COLOR_LOCATION);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
 
 void FramePainter::loadFrame()
 {
-    // m_Program.enableAttributeArray(VERTEX_LOCATION);
-    // m_Program.enableAttributeArray(COLOR_LOCATION);
-
-    glBindVertexArray(m_VertexArraysObject[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, m_PointsBuffer[0]);
-
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_real_distribution<double> dis(-1.0, 1.0);
@@ -91,20 +91,49 @@ void FramePainter::loadFrame()
     {
         m_pColor[i * 3 + 0] = QVector4D(0.8, 0.0, 0.0, 1.0);
         m_pColor[i * 3 + 1] = QVector4D(0.8, 0.0, 0.0, 1.0);
-        m_pColor[i * 3 + 2] = QVector4D(0.8, 0.0, 0.0, 1.0);
+        m_pColor[i * 3 + 2] = QVector4D(0.0, 0.0, 0.8, 1.0);
     }
 
-    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(QVector4D), nullptr, GL_STATIC_DRAW);
+    {
+        m_pElement[0] = 0;
+        m_pElement[1] = 1;
+        m_pElement[2] = -1;
+        m_pElement[3] = 2;
+        m_pElement[4] = 3;
+        m_pElement[5] = -1;
+        m_pElement[6] = 4;
+        m_pElement[7] = 5;
+    }
 
-    glBufferSubData(GL_ARRAY_BUFFER, 0, 6 * sizeof(QVector4D), m_pTriangleVertices);
-    glBufferSubData(GL_ARRAY_BUFFER, 6 * sizeof(QVector4D), 6 * sizeof(QVector4D), m_pColor);
+    loadFrame(m_pTriangleVertices, m_pColor, 6, m_pElement, 8);
+}
+
+void FramePainter::loadFrame(QVector4D* pVertices, QVector4D* pColors, unsigned int verticesSize,
+    unsigned short* pElement, unsigned int elementSize)
+{
+    /*
+    传输绘制索引到 opengl 服务器上的缓存上
+    */
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ElementBuffer[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementSize * sizeof(GLushort), pElement, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    /*
+    传输顶点和颜色到 opengl 服务器的缓存上
+    */
+    glBindVertexArray(m_VertexArraysObject[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, m_PointsBuffer[0]);
+
+    glBufferData(GL_ARRAY_BUFFER, 2 * verticesSize * sizeof(QVector4D), nullptr, GL_STATIC_DRAW);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, verticesSize * sizeof(QVector4D), pVertices);
+    glBufferSubData(GL_ARRAY_BUFFER, verticesSize * sizeof(QVector4D), verticesSize * sizeof(QVector4D), pColors);
 
     glVertexAttribPointer(VERTEX_LOCATION, 4, GL_FLOAT, GL_FALSE, 0, nullptr);
-    glVertexAttribPointer(COLOR_LOCATION, 4, GL_FLOAT, GL_FALSE,
-        sizeof(m_pTriangleVertices), (const GLvoid *) (6 * sizeof(QVector4D)));
+    glVertexAttribPointer(COLOR_LOCATION, 4, GL_FLOAT, GL_FALSE, 0, (const GLvoid *) (verticesSize * sizeof(QVector4D)));
 
-    // glBindBuffer(GL_ARRAY_BUFFER, 0);
-    // glBindVertexArray(0);
-    //m_Program.disableAttributeArray(VERTEX_LOCATION);
-    //m_Program.disableAttributeArray(COLOR_LOCATION);
+    glDisableVertexAttribArray(VERTEX_LOCATION);
+    glDisableVertexAttribArray(COLOR_LOCATION);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
--- GUI/framepainter.h
+++ GUI/framepainter.h
@@ -13,11 +13,15 @@ public:
 
 public:
     void loadFrame();
+    void loadFrame(QVector4D* pVertices, QVector4D* pColors, unsigned int verticesSize,
+        unsigned short* pElement, unsigned int elementSize);
 
 private:
     GLuint m_VertexArraysObject[1];
     GLuint m_PointsBuffer[1];
+    GLuint m_ElementBuffer[1];
 
     QVector4D *m_pTriangleVertices;
     QVector4D *m_pColor;
+    GLushort *m_pElement;
 };
